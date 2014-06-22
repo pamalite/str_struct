e0bffa3768f9b5daa1595fe10115ed96fdcbb606
Pamalite Kensington <pamalite@users.noreply.github.com>
2014-06-22 19:50:24 +0800

    updated str_driver.c to reflect the updates on str_struct

--- str_driver.c
+++ str_driver.c
@@ -1,145 +1,130 @@
- 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 #include "str_struct.h"
 
-int main(int argc, char *argv[]) {
-    Str *testStr1 = Str_new();
-    Str *testStr2 = Str_newWith("hello world!");
-    
-    testStr1->set(testStr1, "bye world!");
-    
-    printf("%s\n", testStr2->str);
-    printf("%s\n", testStr1->str);
-    
-    char *temp = NULL;
-    temp = (char *) calloc(10, sizeof(char));
-    int i;
-    for (i=0; i < 10; i++) {
-        sprintf(temp, "loop %d", i);
-        testStr1->set(testStr1, temp);
-        printf("%s\n", testStr1->str);
-    }
-    
-    Str_dispose(testStr2);
-    Str_dispose(testStr1);
-    
-    Str *strA = Str_newWith("a big brown fox jumped over the lazy dog.");
-    Str *strB = Str_newWith("a big brown fox jumped over the lazy dog.");
-    
-    if (strA->isEqual(strA, strB)) {
-        printf("matched!\n");
-    }
-    
-    strB->set(strB, "a BiG brown FoX jumped over the LAZY dog.");
-    
-    if (!strA->isEqual(strA, strB)) {
-        printf("no match!!!\n");
-    } 
-    
-    Str_dispose(strA);
-    Str_dispose(strB);
-    
-    strA = Str_newWith("a big brown fox jumped over the lazy dog.");
-    strB = Str_newWith("a BiG brown FoX jumped over the LAZY dog.");
-    
-    if (strA->isEqualIgnoreCase(strA, strB)) {
-        printf("matched\n");
-    } else {
-        printf("no match!!!\n");
-    }
-    
-    strA->toUpper(strA);
-    strB->toLower(strB);
-    
-    printf("%s\n", strA->str);
-    printf("%s\n", strB->str);
-    
-    Str_dispose(strA);
-    Str_dispose(strB);
-    
-    strA = Str_new();
-    for (i=0; i < 10; i++) {
-        strA->setWithFormat(strA, "formatted: %d", i);
-        printf("%s\n", strA->str);
-    }
-    Str_dispose(strA);
-    
-    strB = Str_newWithFormat("this %s of %1.3f", "is life", 3.142677388722);
-    printf("%s\n", strB->str);
-    Str_dispose(strB);
-    
-    strA = Str_newWithFormat("     this %s of %1.3f", "is life", 3.142677388722);
-    printf("%s\n", strA->str);
-    strA->leftTrim(strA);
-    printf("%s\n", strA->str);
-    Str_dispose(strA);
-    
-    strA = Str_newWith("   ");
-    printf("%s<-\n", strA->str);
-    strA->leftTrim(strA);
-    printf("%s<-\n", strA->str);
-    Str_dispose(strA);
-    
-    strA = Str_new();
-    printf("%s<-\n", strA->str);
-    strA->leftTrim(strA);
-    printf("%s<-\n", strA->str);
-    Str_dispose(strA);
-    
-    strA = Str_newWithFormat("     this %s of %1.3f     ", "is life", 3.142677388722);
-    printf("%s<-\n", strA->str);
-    strA->trim(strA);
-    printf("%s<-\n", strA->str);
-    Str_dispose(strA);
-    
-    strA = Str_newWith("a big brown fox jumped over the lazy dog.");
-    printf("%s<-\n", strA->str);
-    printf("%s<-\n", (strA->sub(strA, 3, 0))->str);
-    printf("%s<-\n", (strA->sub(strA, 5, 11))->str);
-    printf("%s<-\n", (strA->sub(strA, 25, 50))->str);
-    Str_dispose(strA);
-    
-    strA = Str_newWith("these");
-    strB = Str_newWith(" strings");
-    strA->append(strA, strB);
-    printf("%s\n", strA->str);
-    Str_dispose(strB);
-    Str_dispose(strA);
-    
-    strA = Str_newWith("a big brown fox jumped over the lazy dog.");
-    if (strA->has(strA, "jum")) {
-        printf("jum: found it\n");
-    }
-    
-    if (!strA->has(strA, "banana")) {
-        printf("banana: not found\n");
-    }
-    
-    printf("%s<-\n", strA->str);
-    // printf("01234567890123456789012345678901234567890\n");
-    int *beginIndices = (int *) calloc(1, sizeof(int));
-    // int *beginIndices = NULL;
-    int count = strA->find(strA, "o", &beginIndices);
-    
-    if (count > 0) {
-        int i;
-        for (i=0; i < count; i++) {
-            // printf("beginIndices[%d]: %d\n", i, beginIndices[i]);
-            if (beginIndices[i] == 0) {
-                printf("%c%c...", strA->str[beginIndices[i]], strA->str[beginIndices[i]+1]);
-            } else if (beginIndices[i]+1 >= strA->length) {
-                printf("...%c%c", strA->str[beginIndices[i]-1], strA->str[beginIndices[i]]);
-            } else {
-                printf("...%c%c%c...", strA->str[beginIndices[i]-1], strA->str[beginIndices[i]], strA->str[beginIndices[i]+1]);
-            }
-        }
-    }
-    printf("\n");
-    free(beginIndices);
-    Str_dispose(strA);
-    
-    return 0;
+#include <stdio.h>
+#include <ctype.h>
+
+int main (int argc, char *argv[]) {
+	Str *str = Str_new();
+
+	str->set(str, "this is a string.");
+	printf("%s - %d\n", str->str, str->length);
+
+	str->set(str, "this is also a string.");
+	printf("%s - %d\n", str->str, str->length);
+
+	str->set(str, "this is a super duper long string. the quick brown fox jumped over the lazy dog. bah bah black sheep have you any wool?");
+	printf("%s - %d\n", str->str, str->length);
+
+	Str_dispose(str);
+
+	// ===
+
+	Str *str2 = Str_newWith("%s %d", "some string", 626);
+
+	printf("%s - %d\n", str2->str, str->length);
+
+	Str_dispose(str2);
+
+	// ===
+
+	Str *str3 = Str_newWith("this is a super duper long string. the quick brown fox jumped over the lazy dog. bah bah black sheep have you any wool?");
+
+	printf("%s - %d\n", str3->str, str->length);
+
+	str3->toUpper(str3);
+	printf("%s - %d\n", str3->str, str->length);
+
+	str3->toLower(str3);
+	printf("%s - %d\n", str3->str, str->length);
+
+	Str_dispose(str3);
+
+	// ===
+
+	Str *str4 = Str_newWith("a string");
+	Str *str5 = Str_newWith("a string");
+
+	if (str4->isEqualsIgnoreCase(str4, str5)) {
+		printf("matched!\n");
+	} else {
+		printf("no match!\n");
+	}
+
+	str5->set(str5, "a new thing");
+	if (str5->isEqualsIgnoreCase(str5, str4)) {
+		printf("matched!\n");
+	} else {
+		printf("no match!\n");
+	}
+
+	Str_dispose(str4);
+	Str_dispose(str5);
+
+	// ===
+
+	Str *str6 = Str_newWith("   some texts go here   ");
+
+	printf("_%s_ - %d\n", str6->str, str6->length);
+
+	str6->trim(str6);
+	printf("_%s_ - %d\n", str6->str, str6->length);
+
+	Str_dispose(str6);
+
+	// ===
+
+	Str *str7 = Str_newWith("the quick brown fox jumped over the lazy dog");
+
+	printf("%s - %d\n", str6->str, str6->length);
+
+	Str *str8 = str7->sub(str7, 5, 8);
+	printf("%s - %d\n", str8->str, str8->length);
+
+	Str_dispose(str8);
+	Str_dispose(str7);
+
+	// ===
+
+	Str *str9 = Str_newWith("the lazy dog");
+	Str *str10 = Str_newWith("the quick brown fox");
+	Str *str11 = Str_newWith(" slides under ");
+
+	str9->append(str9, str11);
+	str9->append(str9, str10);
+
+	printf("%s - %d\n", str9->str, str9->length);
+
+	Str_dispose(str10);
+	Str_dispose(str11);
+	Str_dispose(str9);
+
+	// ===
+
+	Str *fileName = Str_newWith("test_file.txt");
+	Str *fileContents = Str_new();
+
+	fileContents->set(fileContents, "lorem ipsum %d lorem %3.3f", 1277, 4.5667);
+
+	FILE *handle = fopen(fileName->str, "w");
+	fprintf(handle, "%s\n", fileContents->str);
+	fprintf(handle, "number of characters written: %d", fileContents->length);
+	fclose(handle);
+
+	Str_dispose(fileContents);
+
+	Str *readFileContents = Str_new();
+	FILE *fp = fopen(fileName->str, "r");
+
+	int c;
+	while ((c = fgetc(fp)) != EOF) {
+		readFileContents->append(readFileContents, Str_newWith("%c", c));
+	}
+
+	fclose(fp);
+
+	printf("%s\n", readFileContents->str);
+
+	Str_dispose(readFileContents);
+	return 0;
 }
